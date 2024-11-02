072ffe76ba7f5bcc44d30800fb0aa9c7d7ba8b7f
ArchieSachin <archieshah8767@gmail.com>
2024-11-02 19:00:26 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    container:{
+      padding:{
+        DEFAULT : '15px',
+      },
+    },
+    screens:{
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    fontFamily:{
+      primary: 'Gamja Flower',
+      secondary: 'Gamja Flower'
+    },
+    extend: {
+      colors:{
+        primary: {
+          DEFAULT:'#292f36',
+          hover:'#343e4a'
+        },
+        secondary: '#4d5053',
+        accent: {
+          DEFAULT:'#cda274',
+          secondary:'#f4f0ec',
+          hover:'#b88c5d',
+        },
+      },
+    },
   },
   plugins: [],
 }
